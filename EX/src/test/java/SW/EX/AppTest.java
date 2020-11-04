7509d49dce0a6e54ded5deb1118f08ef6ac6c6c2
Giuseppe <Giuseppe@192.168.1.222>
2020-11-04 13:21:00 +0100

    .

--- EX/src/test/java/SW/EX/AppTest.java
+++ EX/src/test/java/SW/EX/AppTest.java
@@ -1,9 +1,13 @@
 package SW.EX;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.Arrays;
 import java.util.List;
 
 import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -16,19 +20,29 @@ public class AppTest {
 		listUtil = new MyListUtil();
 	}
 
-	@Test
-	public void ordineDecrescenteWorks() {
-		System.out.println(dt);
-		List<Integer> L = Arrays.asList(11, 13, 17, 23, 31, 41, 53, 67, 83, 101);
-		listUtil.ordineDecrescente(L);
-		System.out.println(dt);
+	@Before
+	public void printStartTime() {
+		System.out.println("Inizio il test " + DateTime.now());
+	}
+
+	@After
+	public void printFinishTime() {
+		System.out.println("Finisco il test " + DateTime.now() + "\n");
 	}
 
 	@Test
 	public void ordineCrescenteWorks() {
-		System.out.println(dt);
-		List<Integer> L = Arrays.asList(101, 2, 67, 53, 41, 31, 23, 17, 13, 11);
-		listUtil.ordineDecrescente(L);
-		System.out.println(dt);
+
+		List<Integer> L = Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1);
+		assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8), listUtil.ordineCrescente(L));
+
+	}
+
+	@Test
+	public void ordineDecrescenteWorks() {
+
+		List<Integer> L = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+		assertEquals(Arrays.asList(8, 7, 6, 5, 4, 3, 2, 1), listUtil.ordineDecrescente(L));
+
 	}
 }
